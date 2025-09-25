7331a504cdab283dc6bb9829e4090b33b71f6cee
Caballero025 <l20590234@sjuanrio.tecnm.mx>
2025-09-25 17:48:32 -0600

    feat: 20590234, Muestra e inserta posts

--- app.py
+++ app.py
@@ -15,15 +15,49 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
 # Crear las tablas si no existen
 with app.app_context():
     db.create_all()
 
+# Modelo Categoría
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
 
-#Ruta raiz
+# Modelo Post
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=True)
+    category = db.relationship('Category', backref=db.backref('posts', lazy=True))
+
+# Ruta para ver todos los posts
 @app.route('/')
 def index():
-    return 'Hola Mundo'
+    posts = Post.query.all()
+    categories = Category.query.all()
+    return render_template('index.html', posts=posts, categories=categories)
+
+#Ruta /post crear un nuevo post
+@app.route('/post/new', methods=['GET','POST'])
+def add_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        category_id = request.form.get('category_id')
+        new_post = Post(title=title, content=content, category_id=category_id)
+        db.session.add(new_post)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+    
+    #Aqui sigue si es GET
+    categories = Category.query.all()
+    return render_template('create_post.html', categories=categories)
 
 if __name__ == '__main__':
     app.run(debug=True)
